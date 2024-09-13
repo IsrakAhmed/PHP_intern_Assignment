f3bc4f00509f8bb7bf9c0f042f3c0676bda36045
IsrakAhmed <israkahmed7@gmail.com>
2024-09-13 18:20:46 +0600

    routes added for crud operations

--- Task 5__To Do Module/To_Do_Module/routes/web.php
+++ Task 5__To Do Module/To_Do_Module/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\ToDoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// The route for the index page
+Route::get('/', [TodoController::class, 'index']) -> name('index');
+
+// The route for the create page
+Route::get('/create', [TodoController::class, 'create']) -> name('create');
+
+// The route for the store the data
+Route::post('/store', [TodoController::class, 'store']) -> name('store');
+
+// The route for the edit page
+Route::get('/edit', [TodoController::class, 'edit']) -> name('edit');
+
+// The route for the update the data
+Route::put('/update', [TodoController::class, 'update']) -> name('update');
+
+// The route for the delete the data
+Route::delete('/delete', [TodoController::class, 'delete']) -> name('delete');
